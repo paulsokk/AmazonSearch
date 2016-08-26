e8b5d449b6197d336ed5f4276c9fe30a8e29dbbf
Paul <Paul@PAUL>
2016-08-26 11:53:50 +0300

    Add project files.

---
+++ AmazonSearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmazonSearch.Startup))]
+namespace AmazonSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
