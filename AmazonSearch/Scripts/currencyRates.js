cb93ac278dabcf6a59ddfbb6bbcef8b8596994e9
Paul <paulsokk@hotmail.com>
2016-08-29 17:56:01 +0300

    Choosing a currency from the drop down list now converts all prices to chosen currency. (The prices always first convert to USD and then to chosen currency.) All conversion logic is handeled inside currencyRates.js

--- AmazonSearch/Scripts/currencyRates.js
+++ AmazonSearch/Scripts/currencyRates.js
@@ -1,5 +1,16 @@
 ﻿var app_id = "ABC";
+var dataObject;
 
+$(document).ready(function () {
+    $('.productPrice').each(function () {
+        this.innerHTML = (parseFloat(this.innerHTML)/100.0)+" GBP";
+    });
+});
+
+
+///
+//Getting currency rates
+///
 $.ajax({
     url: 'https://openexchangerates.org/api/latest.json?app_id='+app_id,
     dataType: 'json',
@@ -7,15 +18,22 @@ $.ajax({
     cache: 'true',
     success: function (data) {
         
+        dataObject = data;
+
         //How to get a specific rate from json object
-        console.log(data.rates["GBP"]);
+        //console.log(data.rates["GBP"]);
         
         //Building a select element 
         var listHTMLString = "<select>";
         var i = 0;
         //Grabbing all possible currency tags
         $.each(data.rates, function (index, value) {
-            listHTMLString += "<option>" + Object.keys(data.rates)[i] + "</option>";
+            var selectedVal = '';
+            if (Object.keys(data.rates)[i] == 'GBP') {
+                selectedVal = "selected";
+            }
+           
+            listHTMLString += "<option "+ selectedVal +" >" + Object.keys(data.rates)[i] + "</option>";
             //console.log(Object.keys(data.rates)[i]);
             i++;
         });
@@ -23,6 +41,50 @@ $.ajax({
 
         //Placing the created select element to #currency div
         $("#currency").append(listHTMLString);
+
     }
+});
+
+
+
+
+///
+//Currency conversion
+///
+
+var fromCurrency = "GBP";
+var toCurrency;
+var x;
+var y;
+
+//Listen for change
+$('#currency').on('change', function () {
+
+    //Remembers previous currency
+    if (toCurrency !== undefined) {
+        fromCurrency = toCurrency;
+    }
+
+    //From oldXXX to USD ratio
+    x = 1 / dataObject.rates[fromCurrency];
+
+    toCurrency = $("#currency option:selected").text(); // or $(this).val()
+    console.log(fromCurrency +" to "+ toCurrency);
+
+    $('.productPrice').each(function () {
+
+        //To USD conversion
+        var XXXAmount = parseFloat(this.innerHTML);
+        var USDAmount = XXXAmount * x;
+
+        //From USD to newXXX ratio
+        y = dataObject.rates[toCurrency];
+
+        //To newXXX conversion
+        var toAmount = USDAmount * y;
+
+        //console.log(toAmount);
+        this.innerHTML = (Math.round(toAmount*100)) / 100.0 + " " + toCurrency;
+    });
 });
 
